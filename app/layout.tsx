2ec4ad2d5a4aac796d68955230c3635e9d928fd7
Richard Reed <richard@pushsend.co>
2025-11-12 14:34:16 +0000

    Update Meta Pixel ID

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,7 +56,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1346281410404508');
+            fbq('init', '1570946560588962');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -65,7 +65,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1346281410404508&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1570946560588962&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
